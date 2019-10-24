eadf6db21a8ff90e7002cbb38af42a90ff246cb9
wint3rmute <mateusz.baczek1998@gmail.com>
2019-10-24 20:31:04 +0200

    some progress on gstreamer

--- gstreamer_playground.sh
+++ gstreamer_playground.sh
@@ -1,1 +1,13 @@
-gst-launch-1.0 v4l2src ! tsparse set-timestamps=true ! video/mpegts ! tsdemux ! video/x-h264 ! h264parse disable-passthrough=true ! rtph264pay ! udpsink -v host=127.0.0.1 port=9999
+# -e allows for ctrl-C interruption that will close the video stream gracefully
+
+# Camera -> .flv file
+
+gst-launch-1.0 -e v4l2src ! videoconvert ! x264enc ! flvmux ! filesink location=xyz.flv
+
+# Camera -> x264 encode -> tsmux -> .ts file
+
+gst-launch-1.0 -e v4l2src ! videoconvert ! x264enc ! mpegtsmux ! filesink location=xyz.ts
+
+
+# Microphone -> .mp3
+gst-launch-1.0 -e pulsesrc ! audioconvert ! lamemp3enc target=1 bitrate=64 cbr=true ! filesink location=audio.mp3
