90a33a1ba5c337628a0a2282b5ad9d4ee183c6cb
wint3rmute <mateusz.baczek1998@gmail.com>
2019-10-30 17:21:59 +0100

    some shit

--- gstreamer/gstreamer_final.sh
+++ gstreamer/gstreamer_final.sh
@@ -6,23 +6,11 @@
 
 
 gst-launch-1.0 -e \
-    v4l2src ! videoconvert ! queue ! x264enc bitrate=950 pass=cbr psy-tune=1 tune=zerolatency  !  h264parse !  \
-    mpegtsmux name=mux ! filesink location="bla.ts" sync=false \
-    pulsesrc ! audioconvert ! queue ! lamemp3enc bitrate=50 ! mpegaudioparse disable-passthrough=1 ! audio/mpeg,rate=48000 !   \
+    v4l2src ! videoconvert ! x264enc bitrate=500 pass=cbr psy-tune=1 tune=zerolatency  !  h264parse !  \
+    mpegtsmux name=mux ! filesink location="bla.ts" sync=true \
+    pulsesrc ! audioconvert ! queue ! lamemp3enc bitrate=256 ! mpegaudioparse disable-passthrough=1 ! audio/mpeg,rate=24000 !   \
     mux.
 
 
-# gst-launch-1.0 -e \
-#     v4l2src ! videoconvert ! x264enc tune=zerolatency ! \
-#     mpegtsmux name=mux ! filesink location="bla.mp4" \
-#     pulsesrc !  lamemp3enc bitrate=16 ! mpegaudioparse disable-passthrough=1 ! audio/mpeg,rate=48000 !   \
-#     mux.
 
-
-# gst-launch-1.0 -e v4l2src ! videoconvert ! x264enc bitrate=1000 pass=cbr tune=zerolatency ! mpegtsmux ! filesink location=xyz.ts
-
-
-
-# gst-launch-1.0 -e \
-#     pulsesrc !  lamemp3enc  bitrate=64 ! mpegaudioparse ! audio/mpeg,rate=48000 !  \
-#     mpegtsmux ! filesink location="bla.mp4" sync=false 
+# gst-launch-1.0 -e v4l2src ! videoconvert ! x264enc bitrate=950 ! mpegtsmux ! filesink location=bla.ts
