6a1bc76ebd2c287056331e60da60abd6d5e38473
wint3rmute <mateusz.baczek1998@gmail.com>
2019-12-12 21:32:53 +0000

    ZAWSZE TAM GDZIE TYYYY

--- gstreamer/gstreamer_final.sh
+++ gstreamer/gstreamer_final.sh
@@ -8,12 +8,14 @@
 # check the latency parameter of the muxer! 
 # h265
 
-gst-launch-1.0 -e \
-    v4l2src ! videoconvert ! x264enc bitrate=500 pass=cbr psy-tune=1 tune=zerolatency  !  h264parse !  \
-    mpegtsmux name=mux ! filesink location="bla.ts" sync=true \
-    pulsesrc ! audioconvert ! queue ! lamemp3enc bitrate=256 ! mpegaudioparse disable-passthrough=1 ! audio/mpeg,rate=24000 !   \
-    mux.
+#gst-launch-1.0 -e \
+#    v4l2src ! videoconvert ! x264enc bitrate=500 pass=cbr psy-tune=1 tune=zerolatency  !  h264parse !  \
+#    mpegtsmux name=mux ! filesink location="bla.ts" sync=true \
+#    pulsesrc ! audioconvert ! queue ! lamemp3enc bitrate=256 ! mpegaudioparse disable-passthrough=1 ! audio/mpeg,rate=24000 !   \
+#    mux.
 
 
 
-# gst-launch-1.0 -e v4l2src ! videoconvert ! x264enc bitrate=950 ! mpegtsmux ! filesink location=bla.ts
+#gst-launch-1.0 -e v4l2src ! videoconvert ! x264enc bitrate=100 ! mpegtsmux ! filesink location=bla.ts
+
+raspivid -t 0 -h 720 -w 1080 -fps 25 -hf -b 1000000 -o - | gst-launch-1.0 -v fdsrc fd=0 ! h264parse ! mpegtsmux ! filesink location=bla.ts
