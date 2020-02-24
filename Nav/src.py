10d55c7133a7d69590923d2ab34d701efe7193db
wint3rmute <mateusz.baczek1998@gmail.com>
2020-02-24 20:21:51 +0100

    example pymavlink

--- Nav/src.py
+++ Nav/src.py
@@ -1,3 +1,7 @@
+# Example of arming a vehicle using pymavlink
+# https://gist.github.com/benboughton1/b5b12130f0448ecef928d8a30c84c4e8
+# O boże o kurwa
+
 class QuadroPlane:
     # Todo: write me
     pass
